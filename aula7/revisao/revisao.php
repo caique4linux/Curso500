0983d05641d78fc645658226aa2f89a82e3eda8a
Caique Portela <caique.portela@4linux.com.br>
2020-01-28 15:27:46 -0300

    Aula 6

---
+++ aula7/revisao/revisao.php
@@ -0,0 +1,24 @@
+<html>
+<body>
+<h1><center>Revisão - aula 7</center></h1>
+<h2><center>https://github.com/caique4linux/Curso500</center></h2>
+<br><br>
+<h3>Trabalhando com arrays</h3>
+<a href="https://www.php.net/manual/pt_BR/book.array.php" target="_blank">https://www.php.net/manual/pt_BR/book.array.php</a>
+<br>
+<br>
+<p><b>Banco de dados</b> Coleção de dados armazenados de forma organizada, facilitando a consulta e manipulação.</p>
+<p><b>SGBD</b> Sistema gerenciador de banco de dados, que que intermedia as manipulações possíveis.</p>
+<p><b>Tabela</b> Onde os dados ficar armazenados e organizadas, possuindo colunas que identificam o tipo dados e cada linha é um registro cadastrado. Representa uma entidade (Cliente, Produto, Pedido, etc.)</p>
+<p><b>Indices</b> Ajudam a melhorar o desempenho do banco ao realizar buscar.</p>
+<p><b>Triggers</b> Eventos/ações executadas em determinadas situações que contem intruções em SQL.</p>
+<p><b>Stored procedures</b> Centralização da regras de negócio em uma instrução SQL salva no banco.</p>
+<p><b>Banco de dados relacional</b> Informações armazenadas em tabelas diferentes compostas por linhas e colunas</p>
+<p><b>Coluna (campo)</b> Definem os dados e os tipos a serem armazenadas.</p>
+<p><b>Linha (registro)</b> Colunas preenchidas de forma a "criar" o dado em si</p>
+<p><b>Chave primaria</b> Campo na tabela que serve como identificador de cada registro. É um valor unico para a tabela, não podendo ser repetido nela. Normalmente usado numeros inteiros sequenciais.</p>
+<p><b>Chave estrangeira</b> Campo na tabela que faz referencia a uma chave primaria de outra tabela, criando assim um relacionamento entre elas.</p>
+<p><b>MySql</b> phpMyAdmin</p>
+<p><b>PostgreSQL</b> pgAdmin</p>
+</body>
+</html>
